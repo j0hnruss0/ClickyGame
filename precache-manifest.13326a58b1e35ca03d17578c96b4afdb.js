e95f702d2be5414e3e98e41c59c2ba5ed088e437
Johnny <johnrussoofficial@outlook.com>
2019-08-28 01:34:23 -0400

    Updates

---
+++ precache-manifest.13326a58b1e35ca03d17578c96b4afdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1edcbed08ea8403dfc32e7149779b135",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "99526065f9fb928bf721",
+    "url": "/ClickyGame/static/css/main.527bb7c9.chunk.css"
+  },
+  {
+    "revision": "41a70de2efff55ad47e5",
+    "url": "/ClickyGame/static/js/2.19149902.chunk.js"
+  },
+  {
+    "revision": "99526065f9fb928bf721",
+    "url": "/ClickyGame/static/js/main.250bda8a.chunk.js"
+  },
+  {
+    "revision": "bc2c7fedd85c052526ee",
+    "url": "/ClickyGame/static/js/runtime~main.4f193b10.js"
+  }
+]);
